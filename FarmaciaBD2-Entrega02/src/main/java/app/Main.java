b7c6b1bb70043990e4d937ee570e9788641f7ae9
SantiagoSerrano27 <santiagogonzaloserrano@gmail.com>
2026-06-12 18:27:48 -0300

    Dependencias + Carga de 90 ventas al Ventas.JSON

--- FarmaciaBD2-Entrega02/src/main/java/app/Main.java
+++ FarmaciaBD2-Entrega02/src/main/java/app/Main.java
@@ -14,67 +14,169 @@ public class Main {
 
         ObraSocial osde = new ObraSocial("OSDE", "458796");
 
-        Cliente cliente1 = new Cliente("Serrano", "Santiago", "45000000",
-                new Direccion("San Martin", 123, "Lomas de Zamora", "Buenos Aires"),
-                osde);
-        Empleado empleado1 = new Empleado("Lopez", "Maximiliano", "31111222", "20-31111222-3",
-                new Direccion("Rivadavia", 100, "Lanus", "Buenos Aires"), osde, true);
+        /* EMPLEADOS */
 
-        Empleado empleado2 = new Empleado("Perez", "Ramiro", "41111222", "20-41111222-4",
-                new Direccion("Rivadavia", 100, "Lanus", "Buenos Aires"), osde, false);
-
-
-        Producto producto1 = new Producto(1001, "Ibuprofeno 600mg", "Bayer",
-                                        "Medicamento", 5000);
-
-        Producto producto2 = new Producto(1002, "Shampoo Sedal", "Unilever",
-                                        "Perfumeria", 3500);
-
-        ItemVenta item1 = new ItemVenta(
-                producto1,
-                2,
-                producto1.getPrecio()
+        Empleado empleado1 = new Empleado(
+                "Lopez",
+                "Maximiliano",
+                "31111222",
+                "20-31111222-3",
+                new Direccion("Rivadavia", 100, "Lanus", "Buenos Aires"),
+                osde,
+                true
         );
 
-        ItemVenta item2 = new ItemVenta(
-                producto2,
-                1,
-                producto2.getPrecio()
+        Empleado empleado2 = new Empleado(
+                "Perez",
+                "Ramiro",
+                "41111222",
+                "20-41111222-4",
+                new Direccion("Rivadavia", 120, "Lanus", "Buenos Aires"),
+                osde,
+                false
         );
 
-        List<ItemVenta> items = Arrays.asList(
-                item1,
-                item2
+        Empleado empleado3 = new Empleado(
+                "Gomez",
+                "Lucas",
+                "32111222",
+                "20-32111222-5",
+                new Direccion("Belgrano", 200, "Lanus", "Buenos Aires"),
+                osde,
+                false
         );
 
-        List<Empleado> listaEmpleados = Arrays.asList(empleado1, empleado2);
-        Sucursal sucursal1 = new Sucursal(1,
-                        new Direccion("Hipolito Yrigoyen", 2500, "Lanus", "Buenos Aires")
-                        ,listaEmpleados, empleado1);
+        /* CLIENTES */
 
-        double totalVenta = 0;
+        List<Cliente> clientes = new ArrayList<>();
 
-        for (ItemVenta item : items) {
+        for (int i = 1; i <= 10; i++) {
 
-            totalVenta += item.getTotal();
+            clientes.add(
+                    new Cliente(
+                            "Apellido" + i,
+                            "Cliente" + i,
+                            "4000000" + i,
+                            new Direccion(
+                                    "Calle " + i,
+                                    100 + i,
+                                    "Lanus",
+                                    "Buenos Aires"
+                            ),
+                            osde
+                    )
+            );
         }
 
-        Venta venta1 = new Venta(
-                "2026-05-22",
-                "0001-00000001",
-                totalVenta,
-                "Tarjeta",
-                cliente1,
-                empleado1,
-                empleado2,
-                sucursal1,
-                items
+        /* PRODUCTOS */
 
+        List<Producto> productos = Arrays.asList(
+
+                new Producto(1001,"Ibuprofeno","Bayer","Medicamento",5000),
+                new Producto(1002,"Paracetamol","Bago","Medicamento",3500),
+                new Producto(1003,"Amoxicilina","Roemmers","Medicamento",6000),
+                new Producto(1004,"Aspirina","Bayer","Medicamento",2500),
+                new Producto(1005,"Omeprazol","Bago","Medicamento",4500),
+                new Producto(1006,"Diclofenac","Genomma","Medicamento",4000),
+                new Producto(1007,"Tafirol","Sidus","Medicamento",3000),
+
+                new Producto(2001,"Shampoo Sedal","Unilever","Perfumeria",3500),
+                new Producto(2002,"Desodorante Rexona","Unilever","Perfumeria",2800),
+                new Producto(2003,"Jabon Dove","Unilever","Perfumeria",2000)
         );
+
+        /* SUCURSALES */
+
+        List<Empleado> empleadosSucursal =
+                Arrays.asList(empleado1, empleado2, empleado3);
+
+        Sucursal sucursal1 = new Sucursal(
+                1,
+                new Direccion(
+                        "Hipolito Yrigoyen",
+                        2500,
+                        "Lanus",
+                        "Buenos Aires"
+                ),
+                empleadosSucursal,
+                empleado1
+        );
+
+        Sucursal sucursal2 = new Sucursal(
+                2,
+                new Direccion(
+                        "Mitre",
+                        1800,
+                        "Banfield",
+                        "Buenos Aires"
+                ),
+                empleadosSucursal,
+                empleado1
+        );
+
+        Sucursal sucursal3 = new Sucursal(
+                3,
+                new Direccion(
+                        "Alsina",
+                        900,
+                        "Lomas de Zamora",
+                        "Buenos Aires"
+                ),
+                empleadosSucursal,
+                empleado1
+        );
+
+        /* VENTAS */
 
         List<Venta> ventas = new ArrayList<>();
 
-        ventas.add(venta1);
+        for (int i = 1; i <= 90; i++) {
+
+            Cliente cliente =
+                    clientes.get(i % clientes.size());
+
+            Producto producto =
+                    productos.get(i % productos.size());
+
+            ItemVenta item = new ItemVenta(
+                    producto,
+                    (i % 3) + 1,
+                    producto.getPrecio()
+            );
+
+            List<ItemVenta> items =
+                    Arrays.asList(item);
+
+            double totalVenta = item.getTotal();
+
+            Sucursal sucursal;
+
+            if (i <= 30) {
+                sucursal = sucursal1;
+            } else if (i <= 60) {
+                sucursal = sucursal2;
+            } else {
+                sucursal = sucursal3;
+            }
+
+            Venta venta = new Venta(
+                    "2026-05-" + String.format("%02d", (i % 28) + 1),
+                    String.format(
+                            "%04d-%08d",
+                            sucursal.getPuntoVenta(),
+                            i
+                    ),
+                    totalVenta,
+                    "Tarjeta",
+                    cliente,
+                    empleado1,
+                    empleado2,
+                    sucursal,
+                    items
+            );
+
+            ventas.add(venta);
+        }
 
         Gson gson = new GsonBuilder()
                 .setPrettyPrinting()
